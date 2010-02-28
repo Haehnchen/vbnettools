49e4dbfcfda6ef7aa5f2b3ebd43853fc7488e6b4
Daniel Espendiller <daniel@espendiller.net>
2010-02-28 09:54:05 +0000

    added HamachiBroadcastFixGUI

---
+++ HamachiBroadcastFixGUI/HamachiBroadcastFixGUI.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HamachiBroadcastFixGUI
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(HamachiBroadcastFixGUI))
+        Me.gpInput = New System.Windows.Forms.GroupBox
+        Me.cbInput = New System.Windows.Forms.ComboBox
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.cbOutput = New System.Windows.Forms.ComboBox
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox
+        Me.cmdStart = New System.Windows.Forms.Button
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip
+        Me.tssColorStatus = New System.Windows.Forms.ToolStripStatusLabel
+        Me.tssRunning = New System.Windows.Forms.ToolStripStatusLabel
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel
+        Me.tssLink = New System.Windows.Forms.ToolStripStatusLabel
+        Me.cmdStop = New System.Windows.Forms.Button
+        Me.gpInput.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'gpInput
+        '
+        Me.gpInput.Controls.Add(Me.cbInput)
+        Me.gpInput.Location = New System.Drawing.Point(12, 12)
+        Me.gpInput.Name = "gpInput"
+        Me.gpInput.Size = New System.Drawing.Size(453, 58)
+        Me.gpInput.TabIndex = 0
+        Me.gpInput.TabStop = False
+        Me.gpInput.Text = "Input Device (LAN-Adapter)"
+        '
+        'cbInput
+        '
+        Me.cbInput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbInput.FormattingEnabled = True
+        Me.cbInput.Location = New System.Drawing.Point(18, 19)
+        Me.cbInput.Name = "cbInput"
+        Me.cbInput.Size = New System.Drawing.Size(406, 21)
+        Me.cbInput.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cbOutput)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 76)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(453, 54)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Output Device (VPN-Adapter eg. Hamachi/Tunngle)"
+        '
+        'cbOutput
+        '
+        Me.cbOutput.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbOutput.FormattingEnabled = True
+        Me.cbOutput.Location = New System.Drawing.Point(18, 19)
+        Me.cbOutput.Name = "cbOutput"
+        Me.cbOutput.Size = New System.Drawing.Size(406, 21)
+        Me.cbOutput.TabIndex = 0
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Enabled = False
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(18, 19)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(406, 64)
+        Me.ListBox1.TabIndex = 1
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.ListBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 136)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(453, 96)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Log"
+        '
+        'cmdStart
+        '
+        Me.cmdStart.Location = New System.Drawing.Point(391, 238)
+        Me.cmdStart.Name = "cmdStart"
+        Me.cmdStart.Size = New System.Drawing.Size(76, 26)
+        Me.cmdStart.TabIndex = 3
+        Me.cmdStart.Text = "Start"
+        Me.cmdStart.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tssColorStatus, Me.tssRunning, Me.ToolStripStatusLabel2, Me.tssLink})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 277)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(479, 22)
+        Me.StatusStrip1.TabIndex = 4
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'tssColorStatus
+        '
+        Me.tssColorStatus.Name = "tssColorStatus"
+        Me.tssColorStatus.Size = New System.Drawing.Size(16, 17)
+        Me.tssColorStatus.Text = "   "
+        '
+        'tssRunning
+        '
+        Me.tssRunning.Name = "tssRunning"
+        Me.tssRunning.Size = New System.Drawing.Size(66, 17)
+        Me.tssRunning.Text = "tssRunning"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(321, 17)
+        Me.ToolStripStatusLabel2.Spring = True
+        '
+        'tssLink
+        '
+        Me.tssLink.IsLink = True
+        Me.tssLink.Name = "tssLink"
+        Me.tssLink.Size = New System.Drawing.Size(61, 17)
+        Me.tssLink.Text = "espend.de"
+        '
+        'cmdStop
+        '
+        Me.cmdStop.Location = New System.Drawing.Point(317, 238)
+        Me.cmdStop.Name = "cmdStop"
+        Me.cmdStop.Size = New System.Drawing.Size(68, 26)
+        Me.cmdStop.TabIndex = 5
+        Me.cmdStop.Text = "Stop"
+        Me.cmdStop.UseVisualStyleBackColor = True
+        '
+        'HamachiBroadcastFixGUI
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(479, 299)
+        Me.Controls.Add(Me.cmdStop)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.cmdStart)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.gpInput)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "HamachiBroadcastFixGUI"
+        Me.Text = "HamachiBroadcastFixGUI"
+        Me.gpInput.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents gpInput As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents cbInput As System.Windows.Forms.ComboBox
+    Friend WithEvents cbOutput As System.Windows.Forms.ComboBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents cmdStart As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents cmdStop As System.Windows.Forms.Button
+    Friend WithEvents tssRunning As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents tssColorStatus As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents tssLink As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As System.Windows.Forms.ToolStripStatusLabel
+
+End Class
---
+++ HamachiBroadcastFixGUI/HamachiBroadcastFixGUI.vb
@@ -0,0 +1,227 @@
+﻿Imports SharpPcap
+Imports SharpPcap.Packets
+Imports System.Net
+
+''' <summary>
+''' The extended Version of HamachiBroadcastFix with GUI and Auto Device Selector.
+''' it will send network broadcast message from one network device to another
+''' Background:
+''' since windows 7 a broadcast message will only send to the first network device out of the
+''' routing table which orders by the metric value under the TCP/IP options.
+''' 
+''' thx to Zolid / Daniel Vinberg for console version
+''' </summary>
+''' <remarks></remarks>
+Public Class HamachiBroadcastFixGUI
+    Dim WithEvents indevice As SharpPcap.LivePcapDevice
+    Private Shared _ipAddress As String
+    Private Shared outdevice As SharpPcap.LivePcapDevice
+    Private Shared alldevices As SharpPcap.LivePcapDeviceList
+    Public Delegate Sub Logger(ByVal LogStr As String)
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Text &= " - " & My.Application.Info.Version.ToString
+        alldevices = LivePcapDeviceList.Instance
+
+        BuildForm()
+        If cbInput.SelectedIndex >= 0 And cbOutput.SelectedIndex >= 0 Then start()
+    End Sub
+
+    Sub BuildForm()
+        For Each device In alldevices
+
+            Dim DevIP As String = GetIP(device.Addresses)
+
+            'input device
+            cbInput.Items.Add(device.Description & " - " & DevIP)
+            If device.Addresses.Count > 0 Then
+                If IsLocalIpAddress(DevIP) = True Then
+                    cbInput.SelectedIndex = cbInput.Items.Count - 1
+                End If
+            End If
+
+            'output device
+            cbOutput.Items.Add(device.Description & " - " & DevIP)
+            If device.Addresses.Count > 0 Then
+                If device.Description.ToLower.Contains("hamachi") Or DevIP.StartsWith("5.") Or _
+                   device.Description.ToLower.Contains("tunngle") Or DevIP.StartsWith("7.") Then
+                    cbOutput.SelectedIndex = cbOutput.Items.Count - 1
+                End If
+            End If
+
+        Next
+
+        'load settings
+        If Not My.Settings.outdeviceText = "" Then
+            If cbOutput.Items.Contains(My.Settings.outdeviceText) Then
+                cbOutput.SelectedIndex = cbOutput.Items.IndexOf(My.Settings.outdeviceText)
+            End If
+        End If
+
+        If Not My.Settings.indeviceText = "" Then
+            If cbInput.Items.Contains(My.Settings.indeviceText) Then
+                cbInput.SelectedIndex = cbInput.Items.IndexOf(My.Settings.indeviceText)
+            End If
+        End If
+    End Sub
+
+    Private Sub indevice_OnPacketArrival(ByVal sender As Object, ByVal e As SharpPcap.CaptureEventArgs) Handles indevice.OnPacketArrival
+        If TypeOf e.Packet Is UDPPacket Then
+            Dim p As UDPPacket = DirectCast(e.Packet, UDPPacket)
+            If (p.DestinationAddress.ToString.CompareTo("255.255.255.255") = 0) Then
+                p.SourceAddress = System.Net.IPAddress.Parse(_ipAddress)
+                p.ComputeUDPChecksum()
+                p.ComputeIPChecksum()
+                'Dim [date] As DateTime = p.PcapHeader.Date
+                'Dim packetLength As UInt32 = p.PcapHeader.PacketLength
+                outdevice.Open()
+                Try
+                    outdevice.SendPacket(p)
+                    InvokeHelper(GetIP(indevice.Addresses) & " -> " & _ipAddress & " Broadcast packet forwarded successfully.")
+                Catch ex As Exception
+                    InvokeHelper("-- " & ex.Message)
+                    Debug.WriteLine(ex.Message)
+                End Try
+                outdevice.Close()
+
+            End If
+        End If
+    End Sub
+#Region "CustomFunctions"
+    Function GetIP(ByVal Device As System.Collections.ObjectModel.ReadOnlyCollection(Of SharpPcap.PcapAddress))
+        For Each Ip In Device
+            'we only want ipv4 addresses
+            If Ip.Addr.ipAddress.AddressFamily = Sockets.AddressFamily.InterNetwork Then
+                Return Ip.Addr.ipAddress.ToString
+            End If
+        Next
+        Return "0.0.0.0"
+    End Function
+    Sub AddLogText(ByVal str As String)
+        ListBox1.Items.Add(Date.Now.ToString("H:mm") & ": " & str)
+        If ListBox1.Items.Count > 5 Then ListBox1.Items.RemoveAt(0)
+    End Sub
+    Sub InvokeHelper(ByVal str As String)
+        'we dont wont kill our app if invoke fails
+        Try
+            Invoke(New Logger(AddressOf AddLogText), str)
+        Catch ex As Exception
+            Debug.WriteLine(ex.Message)
+        End Try
+    End Sub
+    Sub Start()
+        Try
+            If cbInput.SelectedIndex < 0 Then
+                MsgBox("Select Input Device")
+                Exit Sub
+            End If
+
+            If cbOutput.SelectedIndex < 0 Then
+                MsgBox("Select Output Device")
+                Exit Sub
+            End If
+
+            'get the right device
+            outdevice = alldevices.Item(cbOutput.SelectedIndex)
+            indevice = alldevices.Item(cbInput.SelectedIndex)
+            _ipAddress = GetIP(outdevice.Addresses)
+
+            'settings
+            My.Settings.outdeviceText = cbOutput.Items(cbOutput.SelectedIndex)
+            My.Settings.indeviceText = cbInput.Items(cbInput.SelectedIndex)
+            My.Settings.Save()
+
+
+            AddHandler indevice.OnPacketArrival, New PacketArrivalEventHandler(AddressOf indevice_OnPacketArrival)
+            AddLogText("Listening on " & GetIP(indevice.Addresses) & " sending to " & _ipAddress)
+
+            'start capturing
+            indevice.Open()
+            indevice.StartCapture()
+
+            'Form handling
+            tssColorStatus.BackColor = Color.Green
+            tssRunning.Text = "Running..."
+            cmdStart.Enabled = False
+            cmdStop.Enabled = True
+
+        Catch ex As Exception
+            InvokeHelper("-- " & ex.Message)
+            Debug.WriteLine(ex.Message)
+        End Try
+    End Sub
+    Sub Stopit()
+        tssRunning.Text = "Stopped"
+        tssColorStatus.BackColor = Color.Red
+        cmdStop.Enabled = False
+        cmdStart.Enabled = True
+
+        If Not indevice Is Nothing Then
+            Try
+                AddLogText("Stopped")
+                indevice.StopCapture()
+                indevice.Close()
+                indevice = Nothing
+            Catch ex As Exception
+                Debug.WriteLine(ex.Message)
+            End Try
+        End If
+    End Sub
+    ''' <summary>
+    ''' The following method checks if a given host name or IP address is local. First, it gets
+    ''' all IP addresses of the given host, then it gets all IP addresses of the local computer
+    ''' and finally it compares both lists. If any host IP equals to any of local IPs, the host
+    ''' is a local IP. It also checks whether the host is a loopback address (localhost / 127.0.0.1).
+    ''' 
+    ''' http://www.csharp-examples.net/local-ip/
+    ''' </summary>
+    ''' <param name="host">IP to check</param>
+    ''' <returns>true on local addresses</returns>
+    ''' <remarks></remarks>
+    Public Shared Function IsLocalIpAddress(ByVal host As String) As Boolean
+        Try
+            ' get host IP addresses
+            Dim hostIPs As IPAddress() = Dns.GetHostAddresses(host)
+            ' get local IP addresses
+            Dim localIPs As IPAddress() = Dns.GetHostAddresses(Dns.GetHostName())
+
+            ' test if any host IP equals to any local IP or to localhost
+            For Each hostIP As IPAddress In hostIPs
+                ' is localhost
+                If IPAddress.IsLoopback(hostIP) Then
+                    Return True
+                End If
+                ' is local address
+                For Each localIP As IPAddress In localIPs
+                    If hostIP.Equals(localIP) Then
+                        Return True
+                    End If
+                Next
+            Next
+        Catch ex As Exception
+        End Try
+        Return False
+    End Function
+#End Region
+    Private Sub cmdStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdStart.Click
+        start()
+    End Sub
+    Private Sub cmdStop_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdStop.Click
+        Stopit()
+    End Sub
+    Private Sub cbOutput_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbOutput.SelectedIndexChanged
+        Stopit()
+    End Sub
+    Private Sub cbInput_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbInput.SelectedIndexChanged
+        Stopit()
+    End Sub
+    Private Sub HamachiBroadcastFixGUI_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        End
+    End Sub
+    Private Sub HamachiBroadcastFixGUI_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        Stopit()
+    End Sub
+    Private Sub tssLink_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tssLink.Click
+        System.Diagnostics.Process.Start("http://www.espend.de/node/127/?version=" & My.Application.Info.Version.ToString)
+    End Sub
+End Class
