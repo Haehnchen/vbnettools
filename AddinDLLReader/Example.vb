08ed38f361b9b4830909937e43e587555e8ea078
Daniel Espendiller <daniel@espendiller.net>
2010-03-25 13:44:43 +0000

    A class that can dynamically include a remote .NET Framework dll and call some functions at application runtime

---
+++ AddinDLLReader/Example.vb
@@ -0,0 +1,61 @@
+﻿'$Id$
+'$HeadURL$'
+
+Module Example
+
+    Sub Main()
+        'Our DLL has the Namespace ExampleDLL.TheClass, ExampleDLL ist generated automatically out of the filename
+        Dim Addin As New AddinDLLReader("ExampleDLL.dll", "TheClass")
+
+        'Some ready functions
+        Console.WriteLine("Description: " & Addin.Description)
+        Console.WriteLine("Version: " & Addin.Version)
+
+        'call a known function that returns a ArrayList
+        Console.WriteLine(vbNewLine & "GetExtension; returns ArrayList:")
+        For Each Ext In Addin.RunMethod("GetExtension")
+            Console.WriteLine(Ext)
+        Next
+
+        'show all function / sub of the class
+        Console.WriteLine(vbNewLine & "The class contains this methodes:")
+        For Each Met In Addin.GetMethodes
+            Console.WriteLine(Met)
+        Next
+
+        'check if the class has a given function
+        Console.WriteLine(vbNewLine & "checking if function/sub exists")
+        If Addin.GetMethodes.Contains("Version") Then
+            Console.WriteLine("Function ´Version´ was found")
+        End If
+
+        'call a function with a paramter
+        Console.WriteLine(vbNewLine & "We are calling FunctionWithParamter with a single paramter:")
+        Console.WriteLine("returns: " & Addin.RunMethod("FunctionWithParamter", New Object() {"MyParamter"}))
+
+        Console.ReadKey()
+
+
+        'the code above gave you this output
+        'Description: Wow a Description at: 25.03.2010 14:31:36
+        'Version: 1.0.0.0
+
+        'GetExtension; returns ArrayList:
+        '        pdf()
+        '        doc()
+
+        'The class contains this methodes:
+        '        GetExtension()
+        '        Version()
+        '        Description()
+        '        FunctionWithParamter()
+
+        'checking if function/sub exists
+        'Function ´Version´ was found
+
+        'We are calling FunctionWithParamter with a single paramter:
+        'returns: You gave me a nice paramter: MyParamter
+
+    End Sub
+
+End Module
---
+++ AddinDLLReader/ExampleDLL.vb
@@ -0,0 +1,19 @@
+﻿'$Id$
+'$HeadURL$
+
+Public Class TheClass
+    Function GetExtension() As ArrayList
+        Dim Ext As New ArrayList
+        Ext.Add("pdf") : Ext.Add("doc")
+        Return Ext
+    End Function
+    Function Version() As String
+        Return My.Application.Info.Version.ToString()
+    End Function
+    Function Description() As String
+        Return "Wow a Description at: " & Now
+    End Function
+    Function FunctionWithParamter(ByVal paramter As String) As String
+        Return "You gave me a nice paramter: " & paramter
+    End Function
+End Class
